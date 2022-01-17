be84d60962eb0182ce542974f5b1c28831f482cf
momaju <momaju@gmail.com>
2022-01-17 09:21:50 -0300

    Another way to filter location

--- vaccines.R
+++ vaccines.R
@@ -30,3 +30,32 @@ owid_selected <- owid %>% select(location, date,
   select(-date)
 
 owid_selected
+
+# another way to filter location
+
+owid_selected <- owid %>% select(location, date,
+                                 all_vax = people_vaccinated_per_hundred,
+                                 fully_vax = people_fully_vaccinated_per_hundred,
+                                 boosted = total_boosters_per_hundred) %>%
+  
+  # people_vaccinated_per_hundred = Total number of people who received at least one vaccine dose per 100 people in the total population
+  # people_fully_vaccinated_per_hundred = Total number of people who received all doses prescribed by the vaccination protocol per 100 people in the total population
+  # total_boosters_per_hundred = Total number of COVID-19 vaccination booster doses administered per 100 people in the total population
+  
+  # filter(location == "Brazil") %>%
+  # drop_na(fully_vax) %>% slice_max(date)
+  
+  filter((location %in% c("Portugal", "Brazil", "Netherlands", "China", "France", "Germany",
+                          "India", "Italy", "Japan", "Mexico", "South Africa", "South Korea",
+                          "Spain", "United Kingdom", "United States", "Argentina"))) %>%
+  drop_na(fully_vax) %>% 
+  group_by(location) %>%
+  slice_max(date, n=1) %>%
+  ungroup() %>%
+  select(-date)
+
+owid_selected
+
+
+
+

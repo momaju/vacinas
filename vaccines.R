d29320f7b32276e25913492aefc49950557c4951
momaju <momaju@gmail.com>
2022-01-17 10:36:55 -0300

    date included in the selection

--- vaccines.R
+++ vaccines.R
@@ -69,8 +69,8 @@ owid_south_america <- owid %>% select(location, date,
   drop_na(fully_vax) %>% 
   group_by(location) %>%
   slice_max(date, n=1) %>%
-  ungroup() %>%
-  select(-date)
+  ungroup()
+  #select(-date)
 
 owid_south_america
 
